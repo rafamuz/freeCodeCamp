bfce391ac78b2e54e6406f6f3a05fad3b920e7c5
Rafael Muzel <rafaelmuzel@yahoo.com.br>
2023-02-26 20:45:43 -0300

    Created new file with Roman Numerals Conversor chalenge

---
+++ javascript/romanNumeralsConversor.js
@@ -0,0 +1,71 @@
+function convertToRoman(num) {
+ let calculatedNum = num
+ let romanNum = [] 
+  for(let i = Math.floor(num/1000); i > 0; i--) {
+    romanNum.push('M')
+    calculatedNum -= 1000
+  }  
+  for(let i = Math.floor(calculatedNum/900); i > 0; i--)
+  {
+    romanNum.push('CM')
+    calculatedNum -= 900  
+  }
+  for(let i = Math.floor(calculatedNum/500); i > 0; i--)
+  {
+    romanNum.push('D')
+    calculatedNum -= 500  
+  }
+  for(let i = Math.floor(calculatedNum/400); i > 0; i--)
+  {
+    romanNum.push('CD')
+    calculatedNum -= 400  
+  }
+  for(let i = Math.floor(calculatedNum/100); i > 0; i--)
+  {
+    romanNum.push('C')
+    calculatedNum -= 100  
+  }
+  for(let i = Math.floor(calculatedNum/90); i > 0; i--)
+  {
+    romanNum.push('XC')
+    calculatedNum -= 90  
+  }
+  for(let i = Math.floor(calculatedNum/50); i > 0; i--)
+  {
+    romanNum.push('L')
+    calculatedNum -= 50  
+  }
+  for(let i = Math.floor(calculatedNum/40); i > 0; i--)
+  {
+    romanNum.push('XL')
+    calculatedNum -= 40  
+  }
+  for(let i = Math.floor(calculatedNum/10); i > 0; i--)
+  {
+    romanNum.push('X')
+    calculatedNum -= 10  
+  }
+  for(let i = Math.floor(calculatedNum/9); i > 0; i--)
+  {
+    romanNum.push('IX')
+    calculatedNum -= 9  
+  }
+  for(let i = Math.floor(calculatedNum/5); i > 0; i--)
+  {
+    romanNum.push('V')
+    calculatedNum -= 5  
+  }
+  for(let i = Math.floor(calculatedNum/4); i > 0; i--)
+  {
+    romanNum.push('IV')
+    calculatedNum -= 4
+  }
+  for(let i = Math.floor(calculatedNum/1); i > 0; i--)
+  {
+    romanNum.push('I')
+    calculatedNum -= 1  
+  }  
+ return romanNum.join('')
+}
+
+console.log(convertToRoman(7))
